a260fc45227077919de2eff437b3014c85f61202
tuananh <tuananh091299@gmail.com>
2020-10-29 22:03:17 +0700

    commit giao diện home, header, item

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,79 @@
+<template>
+<div>
+    <div class="header">
+    <v-row no-gutters>
+      <v-col cols="3" style="color:white;">
+         KAMI
+        <v-card
+          class="pa-2"          
+          tile
+          style="width:fit-content;"
+        >
+         COMUNITY
+        </v-card>
+      </v-col>
+      <v-col cols="5">
+      </v-col>
+      <v-col cols="2" style="margin: auto;">
+        <v-btn
+        depressed
+        small
+        style="background-color:crimson;color:white;font-family:inherit;"
+        >
+          <ModelLogin></ModelLogin>
+        </v-btn>
+        <v-btn
+        depressed
+        small
+        style="background-color:crimson;color:white;margin-left:5px;font-family:inherit;"
+        >
+          <v-icon>{{icons.mdiAccount}}</v-icon> login
+        </v-btn>
+      </v-col>
+      <v-col cols="2"  style="margin: auto;">
+          <div style="">
+            <v-icon class="1" style="padding-right: 20px;color: rgba(243, 233, 233, 0.54);">fas fa-search</v-icon>
+            <v-icon class="2"  style="color:rgb(243 233 233 / 54%);">fas fa-list</v-icon>
+          </div>
+      </v-col>
+    </v-row>
+  </div>
+  <div>
+    <v-row no-gutters
+    style="height:90px;">
+      <div style="margin:auto;">
+        QUẢNG CÁO
+      </div>
+    </v-row>
+  </div>
+</div>
+</template>
+<script>
+ import ModelLogin from './Login'
+ import {
+    mdiAccount,
+    mdiPencil,
+    mdiShareVariant,
+    mdiDelete,
+  } from '@mdi/js'
+export default {
+    components:{
+      ModelLogin,
+    },
+    data: () => ({
+      icons: {
+        mdiAccount,
+        mdiPencil,
+        mdiShareVariant,
+        mdiDelete,
+      },
+    }),
+  }
+</script>
+
+<style>
+.header{
+ line-height: 30px;
+ background-color: #333;
+}
+</style>
---
+++ frontend/src/components/body.vue
@@ -0,0 +1,94 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col cols="2">
+             <v-combobox
+                 v-model="select"
+                    :items="items"
+                    multiple
+                    outlined
+                    dense
+                >
+             </v-combobox>
+            </v-col>
+            <v-col cols="4">
+              <v-btn
+                depressed
+                large
+                style="background-color:crimson;color:white;font-family:inherit;"
+              >
+                Latest
+              </v-btn>  
+              <v-btn
+                depressed
+                large
+                outlined
+                style="font-family:inherit;"
+              >
+                Categories
+              </v-btn>  
+              <v-btn
+                depressed
+                large
+                outlined
+                style="font-family:inherit;"
+              >
+                TOP
+              </v-btn>  
+            </v-col>
+        </v-row>
+        <v-row style="
+        border-bottom: black;
+         border-bottom: groove;
+         border-bottom-width: unset;
+          ">
+          <v-col cols="9">
+            Topic
+          </v-col>
+          <v-col cols="1">
+            Replies
+          </v-col>
+          <v-col cols="1">
+            Views
+          </v-col>
+          <v-col cols="1">
+            Activity
+          </v-col>                        
+        </v-row>
+        <v-row style="
+         border-bottom: groove;
+         border-bottom-width: thin;
+          ">
+          <v-col cols="9">
+            <p>How to know if my website’s structure is perfect?</p>
+            <div class="contentt">
+             <v-icon> fas fa-circle</v-icon>   HTML & CSS
+            </div>
+          </v-col>
+          <v-col cols="1">
+            4
+          </v-col>
+          <v-col cols="1">
+            100
+          </v-col>
+          <v-col cols="1">
+            18m
+          </v-col>                        
+        </v-row>        
+    </v-container>
+</template>
+<script>
+export default {
+    data () {
+      return {
+        select: ['All categories'],
+        items: [
+          'Programming',
+          'Design',
+          'Vue',
+          'Vuetify',
+        ],
+      }
+    },
+}
+</script>
