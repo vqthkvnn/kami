7029de7ec254aa99492ddf3b11779f67b3f2dd0c
vqhtuyen <hoangtuyensk@gmail.com>
2020-11-09 23:30:52 +0700

    commit 404

--- frontend/src/views/NotFound.vue
+++ frontend/src/views/NotFound.vue
@@ -1,10 +1,16 @@
 <template>
-
+<default-layout>
+  <h3>Sorry</h3>
+  <br/>
+  <h1>404</h1>
+</default-layout>
 </template>
 
 <script>
+import DefaultLayout from "@/components/DefaultLayout";
 export default {
-  name: "NotFound"
+  name: "NotFound",
+  components: {DefaultLayout}
 }
 </script>
 
